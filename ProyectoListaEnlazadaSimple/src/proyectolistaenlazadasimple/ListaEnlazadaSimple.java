3989474164445cd4198c1369317f843dc011cab4
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-10-27 12:26:48 -0700

    linked list

--- ProyectoListaEnlazadaSimple/src/proyectolistaenlazadasimple/ListaEnlazadaSimple.java
+++ ProyectoListaEnlazadaSimple/src/proyectolistaenlazadasimple/ListaEnlazadaSimple.java
@@ -3,17 +3,21 @@ package proyectolistaenlazadasimple;
 public class ListaEnlazadaSimple {
 
     Nodo primero;
+    Nodo ultimo;
     int tamaño;
     
     //Constructor
     public ListaEnlazadaSimple() {
         primero = null;
+        ultimo = null;
         tamaño = 0;
     }
-    //Método para saber el tamaño de lista 
+
+    //Método para saber el tamaño de lista
     public int tamaño() {
         return tamaño;
     }
+
     //Método para Insertar elementos al inicio de la lista 
     public void insertarInicio(Object valor) {
         Nodo nuevo = new Nodo(valor);
@@ -21,20 +25,36 @@ public class ListaEnlazadaSimple {
         primero = nuevo;
         tamaño++;
     }
+
+    // Método para insertar elementos al final de la lista.
+    public void insertarUltimo(Object valor) {
+        Nodo nuevo = new Nodo(valor);
+        nuevo.siguiente = ultimo;
+        ultimo = nuevo;
+        tamaño++;
+    }
+
     //Método para obtener los valores de la lista.
     public String obtenerlista() {
         String datos = "";
         Nodo recorrer = primero;
-        while (recorrer != null) {
+        Nodo recorrer2 = ultimo;
+        while (recorrer != null ) {
             datos = datos + recorrer.info + "\n";
             recorrer = recorrer.siguiente;
         }
+        while (recorrer2 != null) {
+            datos = datos + recorrer2.info + "\n";
+            recorrer2 = recorrer2.siguiente;
+        }
         return datos;
     }
+
     //Manejo de direcciones para ver como se enlazan los nodos
     public String obtenerDirecciones() {
         String dato = "";
         Nodo recorrer = primero;
+        Nodo recorrer2 = ultimo;
         int contador = 1;
         while (recorrer != null) {
             dato += " \n Nodo " + contador
@@ -45,8 +65,17 @@ public class ListaEnlazadaSimple {
             recorrer = recorrer.siguiente;
             contador++;
         }
+        while (recorrer2 != null) {
+            dato += " \n Nodo " + contador
+                    + ": Mi direccion es: " + recorrer2
+                    + " Mi info es: " + recorrer2.info
+                    + "\n \n";
+            recorrer2 = recorrer2.siguiente;
+            contador++;
+        }
         return dato;
     }
+
     //Método para eliminar al final de una lista
     public void EliminarFinal() {
         Nodo penultimo = primero;
@@ -64,5 +93,27 @@ public class ListaEnlazadaSimple {
             tamaño--;
         }
 
+    }
+
+    //  Método para eliminar elemento inicial de la lista.
+    public void EliminarUltimo() {
+        Nodo penultimo = ultimo;
+        Nodo recorrer = ultimo;
+        if (recorrer != null) {
+            if (recorrer.siguiente == null) {
+                primero = null;
+            }
+        } else {
+            primero = null;
+        }
+    }
+
+    //  Método para saber si una lista está vacía
+    public boolean esVacia() {
+        if (tamaño == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- ProyectoListaEnlazadaSimple/src/proyectolistaenlazadasimple/ProyectoListaEnlazadaSimple.java
+++ ProyectoListaEnlazadaSimple/src/proyectolistaenlazadasimple/ProyectoListaEnlazadaSimple.java
@@ -10,12 +10,18 @@ public class ProyectoListaEnlazadaSimple {
         miLista.insertarInicio("Maria");
         miLista.insertarInicio("Juan");
         miLista.insertarInicio("Margarita");
+        miLista.insertarUltimo("Kristopher");
          
         System.out.println("Tamaño de la lista: " + miLista.tamaño());
         System.out.println("Elementos en la Lista: " +"\n" + miLista.obtenerlista());
         
         System.out.println("Direcciones: " + miLista.obtenerDirecciones());
-       
+
+        if (miLista.esVacia()) {
+            System.out.println("Lista vacia");
+        } else {
+            System.out.println("la lista no esta vacia, tiene " + miLista.tamaño() + " elementos");
+        }
     }
     
 }
